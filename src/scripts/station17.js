5ed515161bdc957f6c52f8533b566a208f8e0cd9
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-09 15:20:02 +0900

    stations-2_17

--- src/scripts/station17.js
+++ src/scripts/station17.js
@@ -1,3 +1,13 @@
-handleAlert() {
+function handle() {
   alert("動いたよ！");
 }
+
+
+// コンソールエラーの内容
+// station17.html:10 Uncaught ReferenceError: handle is not defined
+//     at HTMLButtonElement.onclick (station17.html:10:32)
+// onclick	@	station17.html:10
+
+// エラーの原因
+// handleAlert関数が定義されていないため、エラーが発生している。
+// 関数が定義されていないため、エラーが発生している。
